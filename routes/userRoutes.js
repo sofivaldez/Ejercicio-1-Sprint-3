1d77bac74b54bd2d5fd92f30010d326fc2a820d6
Rodrigo Moreira <roke3tres@gmail.com>
2022-08-04 14:54:42 -0300

    Passport

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,10 @@ const userRouter = express.Router();
 const methodOverride = require("method-override");
 const { showRegister, create, showLogin, login, logout } = require("../controllers/userController");
 
-// userRouter.get("/register", showRegister);
-// userRouter.post("/register", create);
-// userRouter.get("/login", showLogin);
-// userRouter.post("/login", login);
+userRouter.get("/register", showRegister);
+userRouter.post("/register", create);
+userRouter.get("/login", showLogin);
+userRouter.post("/login", login);
 // userRouter.get("/logout", logout);
 
 module.exports = userRouter;
