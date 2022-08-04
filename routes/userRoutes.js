7adf462f716257f0a0914bbd6497acc3f0912316
Rodrigo Moreira <roke3tres@gmail.com>
2022-08-04 17:02:49 -0300

    Casi todo listo

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,14 @@ userRouter.get("/register", showRegister);
 userRouter.post("/register", create);
 userRouter.get("/login", showLogin);
 userRouter.post("/login", login);
-// userRouter.get("/logout", logout);
+// userRouter.post("/logout", logout);
+userRouter.post("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect("/");
+  });
+});
 
 module.exports = userRouter;
